c5ad258ad0c210bd74be8c41ae5e5fb7b76ce8c7
Erik Bernhardson <ebernhardson@wikimedia.org>
2026-07-02 10:19:31 -0700

    phrase suggest: Reduce prefix_length to 1
    In a test where were varied both the field content and the prefix
    length of the phrase suggest query we found that the existing
    title + redirec.title content along with reducing prefix_length
    to 1 produced the best results.
    
    Patch removes the profiles that were used for testing, and sets
    the default profile to the updated prefix_length. The strict
    profile was left at a prefix_length of 2 which feels aligned
    with the intent of the profile.
    
    Bug: T390858
    Change-Id: I5fecbfec39faa5caf02e096be18596f1ffb5f1fc

--- profiles/PhraseSuggesterProfiles.config.php
+++ profiles/PhraseSuggesterProfiles.config.php
@@ -69,7 +69,7 @@ return [
 		// candidate suggestions. Defaults to 1. Increasing this number
 		// improves spell check performance.  Usually misspellings don’t
 		// occur in the beginning of terms.
-		'prefix_length' => 2,
+		'prefix_length' => 1,
 
 		// Checks each suggestion against a specified query to prune
 		// suggestions for which no matching docs exist in the index.
@@ -108,45 +108,5 @@ return [
 				'alpha' => 0.3
 			]
 		]
-	],
-	// Same settings as `default` but with prefix_length => 1
-	'default_1' => [
-		'field' => 'suggest',
-		'total_hits_threshold' => 15000,
-		'mode' => 'always',
-		'num_candidates' => 5,
-		'confidence' => 2.0,
-		'max_errors' => 2,
-		'real_word_error_likelihood' => 0.95,
-		'max_term_freq' => 0.5,
-		'min_doc_freq' => 0.0,
-		'prefix_length' => 1,
-		'collate' => false,
-		'collate_minimum_should_match' => '3<66%',
-		'smoothing_model' => [
-			'stupid_backoff' => [
-				'discount' => 0.4
-			]
-		],
-	],
-	// Same settings as `default` but with field => suggest_variant and prefix_length => 1
-	'default_1v' => [
-		'field' => 'suggest_variant',
-		'total_hits_threshold' => 15000,
-		'mode' => 'always',
-		'num_candidates' => 5,
-		'confidence' => 2.0,
-		'max_errors' => 2,
-		'real_word_error_likelihood' => 0.95,
-		'max_term_freq' => 0.5,
-		'min_doc_freq' => 0.0,
-		'prefix_length' => 2,
-		'collate' => false,
-		'collate_minimum_should_match' => '3<66%',
-		'smoothing_model' => [
-			'stupid_backoff' => [
-				'discount' => 0.4
-			]
-		],
 	],
 ];
